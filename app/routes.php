9de3b2ba03744a580b5cd2f00c238fc39cd90f36
ciaorachel <ciaorachel@gmail.com>
2015-08-19 11:44:43 -0500

    finished exercise on views in laravel

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,43 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+Route::get('/resume', function()
+{
+    return "This page is for my resume...";
+});
+
+Route::get('/portfolio', function()
+{
+    return "This page is for my portfolio...";
+});
+
+Route::get('/sayhello/{name?}', function($name)
+{
+    if ($name == "Chris") {
+        return Redirect::to('/');
+    } else {
+        $data = array('name' => $name);
+        return View::make('my-first-view')->with($data);
+    }
+});
+
+Route::get('/rolldice/{guess}', function($guess)
+{
+    $randomRoll = mt_rand(1, 6);
+
+    if ($guess == $randomRoll) {
+    	$message = 'Good guess!';
+    } else {
+    	$message = 'Guess again!';
+    }
+
+    $data = array(
+    	'randomRoll' => $randomRoll,
+    	'guess' => $guess,
+    	'message' => $message
+    );
+
+    return View::make('roll-dice')->with($data);
+
+});
---
+++ app/views/roll-dice.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Random Dice Roll</title>
+</head>
+<body>
+	<h1>Rolling the dice...</h1>
+	<h2>Your guess: <?php echo $guess; ?></h2>
+    <h2>The roll was: <?php echo $randomRoll; ?></h2>
+    <h2><?php echo $message; ?></h2>
+</body>
+</html>
