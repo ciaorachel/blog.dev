ebacb727a6fccd9ac0f454df4eb2cca0d513a576
ciaorachel <ciaorachel@gmail.com>
2015-10-26 22:24:46 -0500

    added bootstrap files to public, also fixed bootstrap font and css files b/c bootstrap left out my grain glyphicon in v.3.1.1 (so now my navbar brand is displaying properly. yay!)

--- app/views/includes/navbar.blade.php
+++ app/views/includes/navbar.blade.php
@@ -7,7 +7,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="{{{ action('HomeController@showWelcome') }}}">ciao<span class="glyphicon glyphicon-grain"></span>rachel</a>
+			<a class="navbar-brand" href="{{{ action('HomeController@showWelcome') }}}">ciao<span class="glyphicon glyphicon-grain" aria-hidden="true"></span>rachel</a>
 		</div>
 		<div id="navbar" class="navbar-collapse collapse">
 			<ul class="nav navbar-nav navbar-right">
